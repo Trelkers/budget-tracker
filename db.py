ab8f0349f79e7af3d994fa8fca0732e6667f54cf
Trelkers <kai.treloar@gmail.com>
2026-08-16 13:28:32 +1000

    parameterise get_connection to accept db filename to open other databases

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def get_connection():
-    conn = sqlite3.connect("budget.db")
+def get_connection(db_name="budget.db"):
+    conn = sqlite3.connect(db_name)
     conn.execute("PRAGMA foreign_keys = ON")
     return conn
